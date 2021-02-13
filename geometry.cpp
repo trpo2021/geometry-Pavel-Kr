5544f1b77b489cbea633ac849c6c09568f16d00b
Pavel-Kr <pkrivosheev@bk.ru>
2021-02-13 19:49:18 +0700

    Spaces are now ignored

--- geometry.cpp
+++ geometry.cpp
@@ -3,15 +3,18 @@
 using namespace std;
 int main()
 {
+    char input[80];
     string inp;
-    cin >> inp;
+    cin.getline(input, 80);
+    inp.assign(input);
     string circle = "circle";
     int lp = inp.find('(');
     int rp = inp.find(')');
+    int space = inp.find(' ');
     if (lp == -1 || rp == -1) {
         cout << "Error: parenthesis expected" << endl;
     }
-    int CTrue = inp.compare(0, lp, circle);
+    int CTrue = inp.compare(0, space, circle);
     if (CTrue == 0)
         cout << "circle";
     else
