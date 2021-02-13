52e22cc7e67a96eeb983641eeba2c8572e79bd63
Pavel-Kr <pkrivosheev@bk.ru>
2021-02-13 19:30:43 +0700

    Add circle check

--- geometry.cpp
+++ geometry.cpp
@@ -8,10 +8,13 @@ int main()
     string circle = "circle";
     int lp = inp.find('(');
     int rp = inp.find(')');
-    if(lp==-1||rp==-1){
-    	cout<<"Error: expected parenthesis";
+    if (lp == -1 || rp == -1) {
+        cout << "Error: parenthesis expected" << endl;
     }
     int CTrue = inp.compare(0, lp, circle);
-    
+    if (CTrue == 0)
+        cout << "circle";
+    else
+        cout << "Error: circle expected";
     return 0;
 }
