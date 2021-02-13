70ccad647cef0f438bcfdbac17674f185d72f4c4
Pavel-Kr <pkrivosheev@bk.ru>
2021-02-13 19:25:48 +0700

    Add parenthesis check

--- geometry.cpp
+++ geometry.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include <string>
 using namespace std;
 int main()
 {
+    string inp;
+    cin >> inp;
+    string circle = "circle";
+    int lp = inp.find('(');
+    int rp = inp.find(')');
+    if(lp==-1||rp==-1){
+    	cout<<"Error: expected parenthesis";
+    }
+    int CTrue = inp.compare(0, lp, circle);
+    
     return 0;
 }
