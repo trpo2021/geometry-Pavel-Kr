050d0f9022b9982f69e358f6122635ad653104f3
Кривошеев Павел Борисович <ip017s33@bwp06.csc.local>
2021-02-20 16:42:51 +0700

    Make output

--- geometry.cpp
+++ geometry.cpp
@@ -4,30 +4,37 @@ using namespace std;
 int main()
 {
     char input[80];
+    for (int i = 0; i < 80; i++) {
+        input[i] = ' ';
+    }
     string inp;
     cin.getline(input, 80);
     inp.assign(input);
-    string circle = "circle";
-    int lp = inp.find('(');
-    int rp = inp.find(')');
-    int space = inp.find(' ');
-    if (lp == -1) {
+    int left_par = inp.find('(');
+    int right_par = inp.find(')', left_par);
+    int first_space = inp.find(' ');
+    bool IsDataTrue = true;
+    if (left_par == -1 || right_par == -1) {
         cout << "Error: parenthesis expected" << endl;
+        IsDataTrue = false;
     }
-    int CTrue = inp.compare(0, space, circle) * inp.compare(0, lp, circle);
-    if (CTrue == 0)
-        cout << "circle" << endl;
-    else
+    int CircleTrue = inp.compare(0, first_space, "circle")
+            * inp.compare(0, left_par, "circle")
+            * inp.compare(0, first_space, "CIRCLE")
+            * inp.compare(0, left_par, "CIRCLE");
+    if (CircleTrue != 0) {
         cout << "Error: circle expected" << endl;
-    int zap = inp.find(',', lp);
-    if (zap == -1) {
-        cout << "Error: wrong data" << endl;
+        IsDataTrue = false;
     }
-    bool NTrue = true;
+    int comma = inp.find(',', left_par);
+    if (comma == -1) {
+        cout << "Error: wrong data" << endl;
+        IsDataTrue = false;
+    }
     bool IsNumber = false;
     int lcount = 0; // must be 2
     int rcount = 0; // must be 1
-    for (int i = lp + 1; i <= zap; i++) {
+    for (int i = left_par + 1; i <= comma; i++) {
         if ((inp[i] == ' ' || inp[i] == ',') && IsNumber) {
             lcount++;
             IsNumber = false;
@@ -37,12 +44,13 @@ int main()
         } else if (inp[i] == ' ')
             continue;
         else {
-            NTrue = false;
+            cout << "Error: wrong symbols" << endl;
+            IsDataTrue = false;
             break;
         }
     }
     IsNumber = false;
-    for (int i = zap + 1; i <= rp; i++) {
+    for (int i = comma + 1; i <= right_par; i++) {
         if ((inp[i] == ' ' || inp[i] == ')') && IsNumber) {
             rcount++;
             IsNumber = false;
@@ -52,13 +60,24 @@ int main()
         else if (inp[i] == ' ')
             continue;
         else {
-            NTrue = false;
+            cout << "Error: wrong symbols" << endl;
+            IsDataTrue = false;
             break;
         }
     }
-    if (!NTrue)
-        cout << "Error: wrong symbols" << endl;
-    if (lcount != 2 || rcount != 1)
+    if (lcount != 2 || rcount != 1) {
         cout << "Error: wrong amount of data" << endl;
+        IsDataTrue = false;
+    }
+    for (int i = right_par + 2; i < 80; i++) {
+        if (input[i] != ' ') {
+            cout << "Error: unexpected token" << endl;
+            IsDataTrue = false;
+            break;
+        }
+    }
+    if (IsDataTrue) {
+        cout << inp;
+    }
     return 0;
 }
